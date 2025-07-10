3099a72231e496ace90af031f03db0f75bbc7fa4
Deepankar Sah <deepankerrocking@gmail.com>
2025-07-10 21:56:44 +0530

    components

---
+++ 1.Components/components/app/index.tsx
@@ -0,0 +1,10 @@
+import Greet from "@/components/Greet";
+import { View, Text } from "react-native";
+const index = () => {
+  return (
+    <View>
+      <Greet />
+    </View>
+  );
+};
+export default index;
---
+++ 1.Components/components/components/Greet.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+const Greet = () => {
+  return (
+    <View>
+      <Text>Hello from component</Text>
+    </View>
+  );
+};
+export default Greet;
