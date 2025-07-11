2353d26112527c9f36269fd088b84afcdbb883e3
Deepankar Sah <deepankerrocking@gmail.com>
2025-07-11 16:38:22 +0530

    FlatList Challenges

---
+++ 3. Flat Lists/Flat Lists Challenge/flat-list-challenge/app/index.tsx
@@ -0,0 +1,10 @@
+import HugeItems from "@/components/HugeItems";
+import { View, Text } from "react-native";
+const index = () => {
+  return (
+    <View>
+      <HugeItems />
+    </View>
+  );
+};
+export default index;
